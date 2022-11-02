6081d68f26f47bea411b7fffca7312b54b0ccbc9
rap0at <85862714+rap0at@users.noreply.github.com>
2022-11-02 22:50:42 +0900

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,32 @@
+chmod 777 *
+cp -r bashNmap /usr/local/sbin
+cp -r bashNmap /usr/bin
+cp -r ngrok /usr/local/sbin
+cp -r ngrok_64 /usr/bin
+
+# ---- nginx ---- #
+# sudo apt install nginx -y
+# sudo service apache2 stop
+# sudo service nginx restart
+
+# ---- tor ---- #
+# sudo apt install tor -y
+# sudo cp torrc /etc/tor/torrc
+# sudo cp onion/index.html /var/www/html/index.html
+# sudo service tor restart
+# cat /var/lib/tor/hidden_service/hostname
+
+echo '\e[1;33m
+ _              _     , _                          
+| |            | |   /|/ \                         
+| |   __,   ,  | |    |   |   _  _  _    __,    _  
+|/ \_/  |  / \_|/ \   |   |  / |/ |/ |  /  |  |/ \_
+ \_/ \_/|_/ \/ |   |_/|   |_/  |  |  |_/\_/|_/|__/ 
+                                             /|    
+                                             \|\e[1;34m
+                                
+                                Created by "RAPOAT"
+
+                                                             \e[1;32m
+								\e[0m 
+'
