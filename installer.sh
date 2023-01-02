0755ed1a31b7e7ae02779669a2b7deaab6105aa4
rap0at <85862714+rap0at@users.noreply.github.com>
2023-01-03 00:59:50 +0900

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -4,18 +4,6 @@ cp -r bashNmap /usr/bin
 cp -r ngrok /usr/local/sbin
 cp -r ngrok_64 /usr/bin
 
-# ---- nginx ---- #
-# sudo apt install nginx -y
-# sudo service apache2 stop
-# sudo service nginx restart
-
-# ---- tor ---- #
-# sudo apt install tor -y
-# sudo cp torrc /etc/tor/torrc
-# sudo cp onion/index.html /var/www/html/index.html
-# sudo service tor restart
-# cat /var/lib/tor/hidden_service/hostname
-
 echo '\e[1;33m
  _              _     , _                          
 | |            | |   /|/ \                         
