f7952f31d57483e17620263f7a4ffdaf7ce4b2f0
rap0at <85862714+rap0at@users.noreply.github.com>
2024-10-29 08:40:45 +0900

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,6 +4,10 @@ cp -r bashNmap /usr/bin
 cp -r ngrok /usr/local/sbin
 cp -r ngrok_64 /usr/bin
 apt -y install php unzip git wget
+gobuster
+git clone https://github.com/shibli2700/Kyubi.git
+cd Kyubi && sudo python3 setup.py install && pip3 install -r /opt/nginxpwner/requirements.txt
+pip3 install -r reqrequirements.txt
 ./1.sh
 ./2.sh
 echo '\e[1;33m
